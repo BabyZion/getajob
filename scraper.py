0e43b83fc9c16ce1447a57c7eae88b6c9b0b80d8
BabyZion <sevcen23@gmail.com>
2021-08-20 22:59:07 +0300

    Initial commit of scraper module.

---
+++ scraper.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import requests
+import json
+from bs4 import BeautifulSoup
+
+
+class Scraper:
+
+    def __init__(self):
+        pass
+
+    def get_number_of_job_openings(self):
+        pass
+
+    def get_job_data(self):
+        pass
+
+    def refine_job_ad_data(self):
+        pass
+
+    def refine_job_data(self):
+        pass
+
+
+class CVScraper(Scraper):
+    pass
+
+
+class CVbankasScraper(Scraper):
+    pass
+
+
+class CVonlineScraper(Scraper):
+    pass
+
+
+class GeraPraktikaScraper(Scraper):
+    pass
